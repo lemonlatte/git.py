9b99334acb9b8334f32d2c8c4fa92c24304f82cb
Jim Yeh <lemonlatte@gmail.com>
2013-05-29 13:54:33 +0800

    Add two function
    * git-commit
    * git-log

--- base.py
+++ base.py
@@ -23,25 +23,28 @@ class GitRepo(object):
         print " ".join(cmd)
         proc = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
-        proc.wait()
+        stdout, stderr = proc.communicate()
         if proc.returncode:
-            raise GitCommandError(proc.stderr.read())
-        print proc.stdout.read()
-        print proc.stderr.read()
+            raise GitCommandError(stderr)
+        # print stdout
+        return stdout
 
     def status(self):
-        self._run('status')
+        print self._run('status')
 
     def stage(self, *args):
         if args:
             self._run('add', *args)
+        else:
+            self._run('add', ".")
 
     def unstage(self, *args):
         if args:
             self._run("reset", "HEAD", *args)
 
-    def commit(self, message, *args):
-        self._run('commit', "-m", message, *args)
+    def commit(self, message, body, *args):
+        """Test"""
+        self._run('commit', "-m", "%s\n\n%s" % (message, body), *args)
 
     # @repo_func
     def fetch(self, remote="origin", branch="master", *args):
@@ -55,3 +58,28 @@ class GitRepo(object):
 
     def push(self, remote="origin", branch="master", *args):
         self._run('push', remote, branch)
+
+    def log(self, commit=None):
+        _cmd = ['log', '--pretty=format:"%H,%an,%ad,%s,%b"']
+        if commit:
+            _cmd[1:1] = ['-1', commit]
+            raw_log = self._run(*_cmd)
+            commit_log = GitLog(*raw_log.split(","))
+            return commit_log
+        else:
+            return self._run(*_cmd)
+
+
+class GitLog(object):
+    def __init__(self, commit, author, date, subject, body):
+        self.commit = commit
+        self.author = author
+        self.date = date
+        self.subject = subject
+        self.body = body
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        return "commit %s\nAuthor: %s\nSubject: %s" % (self.commit, self.author, self.subject)
